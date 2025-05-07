05f15ae20b47d9e4ca345dea602967b27b4433b9
Tyrone <tyroneedwards.softwaredev@gmail.com>
2025-05-06 20:38:04 -0400

    small update

--- avalon-test/web-ver/index.ts
+++ avalon-test/web-ver/index.ts
@@ -1,9 +1,18 @@
-import * as express from "express";
-const app = new express();
+import express from "express";
+const app = express();
 
-app.get('/', (req, res)=>{
-    res.send('listening to port 3000');
+app.get("/", (req: any, res: any) => {
+  res.send("listening to port 3000");
 });
+
+app.get("/movies", async (req: any, res: any) => {
+  let resposne = await fetch(
+    `https://www.movies.com/movies?title=${req.params["title"]}`
+  );
+ // console.log(await resposne.text());
+  res.send(await resposne.text());
+});
+
 app.listen(3000, () => {
   console.log("listening to port 3000");
 });
